f55e8fa0f12657fa48833231e2da75f398080e7f
ahuaa560 <96681825+ahuaa560@users.noreply.github.com>
2024-05-22 19:29:39 +0800

    Create OrganizationService.java

---
+++ src/main/java/com/example/examManageFronend1/service/OrganizationService.java
@@ -0,0 +1,19 @@
+package com.example.examManageFronend1.service;
+
+import com.example.examManageFronend1.mapper.OrganizationMapper;
+import com.example.examManageFronend1.model.Organization;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationService {
+
+    private final OrganizationMapper organizationMapper;
+
+    public OrganizationService(OrganizationMapper organizationMapper) {
+        this.organizationMapper=organizationMapper;
+    }
+
+    public Organization findByUserId(String userId) {
+        return organizationMapper.findByUserId(userId);
+    }
+}
